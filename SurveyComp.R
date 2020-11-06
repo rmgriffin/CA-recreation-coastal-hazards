ebefa9c9e6e11499449debb47a33a79dfa5fe3bb
rmgriffin <r.maurice.griffin@gmail.com>
2020-11-05 23:05:43 -0500

    PUD analysis complete for SMC, CA Parks, LA Beaches

--- SurveyComp.R
+++ SurveyComp.R
@@ -15,7 +15,7 @@ renv::snapshot()
 
 ### CA State Parks
 
-## Parks vector
+## Parks vector (annual visitation 2012 - 2016, with annual estimates running from 7/1 to 6/30 of the following year)
 parks<-st_read("./Data/calStateParks_20190225c_polygon_ta.gpkg")
 parks<-subset(parks, select = c(Name,geom))
 
@@ -43,8 +43,6 @@ PUD<-st_transform(PUD, st_crs(parks))
 parks<-st_join(parks,PUD)
 parks<-unique(parks)
 parks[is.na(parks)]<-0
-
-## Running 4 separate calls to python to generate annual flickr visitation numbers for 2012 - 2016 (Visitation is reported from July 1 to June 30 of the following year, therefore the relevant comparison requires us to manually assemble annual PUD for two years by adding up the relevant months)
 
 ## Reading dbfs of InVEST results
 x12<-read.dbf("./Data/pud_results_x12.dbf")
@@ -90,7 +88,7 @@ parks$ihsPUDall<-ihs(parks$PUDall)
 parks$coast<-as.factor(parks$coast)
 
 ## Plot
-# Basic plot of all data
+# Basic plot of all data 
 ggplot(parks, aes(x=ihsxall, y=ihsPUDall, color=coast)) + geom_point() + 
   labs(x="CDPR visitation (IHS transformed)",y="PUD Flickr (IHS transformed)") +
   theme_classic()
@@ -125,4 +123,86 @@ nobs(lm(ihsPUDall ~ ihsxall, data = parks %>% filter(ihsxall > 0)))
 summary(lm(ihsPUDall ~ ihsxall, data = parks %>% filter(ihsxall > 0 & coast == 1)))
 nobs(lm(ihsPUDall ~ ihsxall, data = parks %>% filter(ihsxall > 0 & coast == 1)))
 
-### LA Beaches
+### LA Beaches (annual visitation, 2008 - 2010)
+lab<-read.csv("./Data/LABeaches2010_2012.csv", header = TRUE)
+# lab$ihsPUD<-ihs(lab$PUD)
+# lab$ihsX<-ihs(lab$X)
+
+# ggplot(parks %>% filter(ihsxall > 0 & coast ==1), aes(x=ihsxall, y=ihsPUDall)) + geom_point() + 
+#   labs(x="CDPR visitation (IHS transformed)",y="PUD Flickr (IHS transformed)") +
+#   theme_classic() +
+#   geom_point(data=lab, aes(x=ihsX, y=ihsPUD), color = "#bae4bc")
+
+### San Mateo County parks
+smcp<-read.csv("./Data/SanMateoParks.csv", header = TRUE)
+# smcp$ihsPUD<-ihs(smcp$PUD)
+# smcp$ihsX<-ihs(smcp$visitor.counts)
+
+# ggplot(parks %>% filter(ihsxall > 0 & coast ==1), aes(x=ihsxall, y=ihsPUDall)) + geom_point() + 
+#   labs(x="CDPR visitation (IHS transformed)",y="PUD Flickr (IHS transformed)") +
+#   theme_classic() +
+#   geom_point(data=lab, aes(x=ihsX, y=ihsPUD), color = "#bae4bc") +
+#   geom_point(data=smcp, aes(x=ihsX, y=ihsPUD), color = "blue") +
+#   geom_smooth(method = lm, color = "black") +
+#   geom_smooth(data = lab, formula = ihsX ~ ihsPUD, color = "#bae4bc")
+
+### Combine data and plot
+# The below also standardizes reporting, to annual visits/PUD
+parkscoast0<-parks %>% filter(ihsxall > 0 & coast ==1) # Only using obs on the coast and >0 
+d1<-select(parkscoast0, c(PUDall,Xall)) %>% st_set_geometry(NULL)
+colnames(d1)<-c("PUD","X")
+d1$group<-"CA Parks"
+d1$PUD<-d1$PUD/4 # 4 years of data
+d1$X<-d1$X/4
+d2<-select(lab,c(PUD,X))
+d2$group<-"LA Beaches"
+d3<-select(smcp, c(PUD,visitor.counts))
+colnames(d3)<-c("PUD","X")
+d3$group<-"SMC Parks"
+d3$PUD<-d3$PUD*4 # 3 months of data
+d3$X<-d3$X*4
+df<-rbind(d1,d2,d3)
+
+df$ihsPUD<-ihs(df$PUD)
+df$ihsX<-ihs(df$X)
+
+df$group<-as.factor(df$group)
+
+ggplot(df, aes(x=X, y=PUD, color = group)) + geom_point() + 
+  labs(x="Surveyed total visitation",y="PUD Flickr") +
+  theme_classic()
+
+ggplot(df, aes(x=ihsX, y=ihsPUD, color = group)) + geom_point() + 
+  labs(x="Surveyed total visitation",y="PUD Flickr") +
+  theme_classic() +
+  geom_smooth(method = lm)
+
+# CA Parks = summed total visitation, 2012 - 2016
+# SMC Parks = summed visitation, April through June 2016
+# LA Beaches = annual average visitation, 2008 - 2010
+
+ggplot(df, aes(x=ihsX, y=ihsPUD)) + geom_point() + 
+  labs(x="Surveyed total visitation",y="PUD Flickr") +
+  theme_classic() +
+  geom_smooth(method = lm)
+
+# Ratio of X to PUD, all grouped
+summary(mod<-lm(X ~ PUD, data = df)) # Untransformed
+# https://rpubs.com/iabrady/residual-analysis
+plot(mod, which=1, col=c("blue")) # Residual plot
+plot(mod, which=2, col=c("red")) # Q-Q plot
+shapiro.test(mod[['residuals']])
+plot(mod, which=3, col=c("blue"))  # Scale-Location Plot
+plot(mod, which=5, col=c("blue"))  # Residuals vs Leverage
+nobs(lm(X ~ PUD, data = df))
+
+summary(ihsmod<-lm(ihsX ~ ihsPUD, data = df)) # Inverse hyperbolic sine transformation
+plot(ihsmod, which=1, col=c("blue")) # Residual plot
+plot(ihsmod, which=2, col=c("red")) # Q-Q plot
+shapiro.test(ihsmod[['residuals']])
+plot(ihsmod, which=3, col=c("blue"))  # Scale-Location Plot
+plot(ihsmod, which=5, col=c("blue"))  # Residuals vs Leverage
+nobs(lm(X ~ PUD, data = df))
+
+# QQ plot suggests that the residuals are not normally distributed using raw values, and that in particular there are issues with large values. IHS transformation returns normally distribute residuals
+# Shapiro Wilk test for normality also supports this interpretation
