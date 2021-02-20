da6018408a278f08c4bd993efab68f2a6c5d5d95
rmgriffin <r.maurice.griffin@gmail.com>
2021-02-20 18:45:54 -0500

    Cleaned code, added additional data processing

--- CA-recreation-coastal-hazards.R
+++ CA-recreation-coastal-hazards.R
@@ -20,7 +20,7 @@ rm(p,PKG)
 ## Snapshot of libraries used
 renv::snapshot()
 
-### Downloading data
+## Downloading data
 dir.create(file.path('Data'), recursive = TRUE)
 folder_url<-"https://drive.google.com/open?id=1mBXEDzc2hBnOa2bxKmg6P9Oz8taR9rTt"
 folder<-drive_get(as_id(folder_url))
@@ -35,34 +35,29 @@ file.remove("Data.zip")
 setwd("..")
 rm(files, folder, folder_url, dl)
 
-### Data processing function to run across resolutions
+## Data processing function to run across resolutions
 data.processing<-function(f,t){
 
-  ### Dependent variables
+  # Dependent variables
   Flickr<-st_read(f)
   Flickr$PUD<-Flickr$PUD_YR_AVG*13 # Convert PUD_YR_AVG to PUD
-  # Add id
-  Flickr$id<-seq.int(nrow(Flickr))
+  Flickr$id<-seq.int(nrow(Flickr)) # Add id
   Twitter<-st_read(t)
   Flickr$TUD<-Twitter$PUD_YR_AVG*6 # Convert TUD_YR_AVG to TUD
   Depvar<-Flickr[,c("PUD","TUD","id")]
   
-  ### Independent variables
-  ## ESI
-  ESI<-st_read("Data/ESIL_CA.gpkg")
+  # Independent variables
+  ESI<-st_read("Data/ESIL_CA.gpkg") # ESI
   ESI<-st_transform(ESI, st_crs(Depvar))
   ESI<-ESI[,c("ESI","geom")]
   ESIi<-st_intersection(ESI,Depvar)
   ESIi<-ESIi[,c("ESI","geom")]
   ESIi$length<-st_length(ESIi) # Lengths can be greater than the hexagonal size as some shorelines loop considerably within a hexagon
-  # Spatial join of ESI and Depvar
   df<-st_join(Depvar,ESIi) # Spatial join
   df$ESI<-as.character(df$ESI)
-  # Create a dataframe of only unique values (combinations of classes) within each hexagon
-  df<-unique(df)
+  df<-unique(df) # Create a dataframe of only unique values (combinations of classes) within each hexagon
   df<-separate(df,ESI,c("I1","I2","I3"),"/",convert = TRUE) 
-  # Create variables for all classes and populate them with relevant length
-  df$x1a<-ifelse(df$I1 == "1A",df$length, ifelse(df$I2 == "1A",df$length, ifelse(df$I3 == "1A",df$length,0)))
+  df$x1a<-ifelse(df$I1 == "1A",df$length, ifelse(df$I2 == "1A",df$length, ifelse(df$I3 == "1A",df$length,0))) # Create variables for all classes and populate them with relevant length
   df$x1b<-ifelse(df$I1 == "1B",df$length, ifelse(df$I2 == "1B",df$length, ifelse(df$I3 == "1B",df$length,0)))
   df$x2a<-ifelse(df$I1 == "2A",df$length, ifelse(df$I2 == "2A",df$length, ifelse(df$I3 == "2A",df$length,0)))
   df$x3a<-ifelse(df$I1 == "3A",df$length, ifelse(df$I2 == "3A",df$length, ifelse(df$I3 == "3A",df$length,0)))
@@ -79,167 +74,120 @@ data.processing<-function(f,t){
   df$x9b<-ifelse(df$I1 == "9B",df$length, ifelse(df$I2 == "9B",df$length, ifelse(df$I3 == "9B",df$length,0)))
   df$x10a<-ifelse(df$I1 == "10A",df$length, ifelse(df$I2 == "10A",df$length, ifelse(df$I3 == "10A",df$length,0)))
   df$xU<-ifelse(df$I1 == "U",df$length, ifelse(df$I2 == "U",df$length, ifelse(df$I3 == "U",df$length,0)))
-  # Replace NAs with zeros
-  df[is.na(df)]<-0
-  # Aggregating data by factor (observation is a hexagon, unique by id). "Max" ensures we get a 1, even when there are multiple 1's for the same class per hexagon
+  df[is.na(df)]<-0 # Replace NAs with zeros
   df<-subset(df, select = c("id","x1a","x1b","x2a","x3a","x3b","x4","x5","x6a","x6b","x7","x8a","x8b","x8c","x9a","x9b","x10a","xU"))
   df$geom<-NULL
-  df<-aggregate(. ~ id,df,sum)
-  # Rejoinging aggregated data to hexagons
-  df<-merge(Depvar,df,"id")
-  # Lots of observations that do not overlap shoreline layer
-  df$types<-df$x1a+df$x1b+df$x2a+df$x3a+df$x3b+df$x4+df$x5+df$x6a+df$x6b+df$x7+df$x8a+df$x8b+df$x8c+df$x9a+df$x9b+df$x10a+df$xU
-  df<-df[which(df$types!=0),]
-  #st_write(df,dsn="validation.gpkg",layer="df")
-  # Removing unneeded vars
-  rm(Depvar, ESI)
+  df<-aggregate(. ~ id,df,sum) # Aggregating data by factor (observation is a hexagon, unique by id)
+  df<-merge(Depvar,df,"id") # Rejoinging aggregated data to sf object
+  df$types<-df$x1a+df$x1b+df$x2a+df$x3a+df$x3b+df$x4+df$x5+df$x6a+df$x6b+df$x7+df$x8a+df$x8b+df$x8c+df$x9a+df$x9b+df$x10a+df$xU 
+  df<-df[which(df$types!=0),] # Lots of observations that do not overlap shoreline layer
+  rm(Depvar, ESI) # Removing unneeded vars
   
-  ## Adjacent population derived from EPA population raster
-  Pop<-raster("Data/dasymetric_us_20160208.tif")
-  # Creating a (8 mile) buffered shapefile of hexagons
-  Pop.buff8m<-st_buffer(df,dist=12784)
+  Pop<-raster("Data/dasymetric_us_20160208.tif") # Adjacent population derived from EPA population raster
+  Pop.buff8m<-st_buffer(df,dist=12784) # Creating a (8 mile) buffered shapefile of hexagons
   Pop.buff8m<-st_transform(Pop.buff8m,st_crs(Pop))
-  # Extracting sum of population raster for each buffered polygon 
-  system.time(Pop.buff8m$sumpop<-exact_extract(Pop, Pop.buff8m, "sum"))
-  # Merging to dataframe
-  Pop.buff8m<-Pop.buff8m[,c("id","sumpop")]
+  system.time(Pop.buff8m$sumpop<-exact_extract(Pop, Pop.buff8m, "sum")) # Extracting sum of population raster for each buffered polygon 
+  Pop.buff8m<-Pop.buff8m[,c("id","sumpop")]  # Merging to dataframe
   Pop.buff8m$geometry<-NULL # Turns sf object into dataframe
   df<-merge(df,Pop.buff8m,"id")
-  # Removing unneeded vars
-  rm(Pop,Pop.buff8m)
-  # Rounding to whole individuals
-  df$sumpop<-round(df$sumpop)
+  rm(Pop,Pop.buff8m) # Removing unneeded vars
+  df$sumpop<-round(df$sumpop) # Rounding to whole individuals
   
-  ## Road network distance
-  # Road network line shapefile
-  Rd<-st_read("Data/Roads_2015.gpkg")
+  Rd<-st_read("Data/Roads_2015.gpkg") # Road network distance. Following https://stackoverflow.com/questions/53854803/calculate-minimum-distance-between-multiple-polygons-with-r
   Rd<-st_transform(Rd, st_crs(df))
-  # Nearest distance, following https://stackoverflow.com/questions/53854803/calculate-minimum-distance-between-multiple-polygons-with-r
   system.time(index<-st_nearest_feature(df, Rd)) # Creates an ordered list (index) of closest elements from Rd shapefile to each feature in "df"
   Rd<-Rd %>% slice(index) # Subsets the Rd shapefile by creating a reordered version based on the element ordering of "index"
   df$rdist<-as.vector(st_distance(df, Rd, by_element = TRUE)) # Calculates distance between two sf objects, element-by-element. as.vector removes [m] units
   rm(Rd)
   
-  ## Adding "Yourcoast" data from California Coastal Commission and postprocessing
-  YC<-st_read("Data/coastal_access_locations.gpkg")
+  YC<-st_read("Data/coastal_access_locations.gpkg") # Adding "Yourcoast" data from California Coastal Commission and postprocessing. Nearest calculation follows same process as road network approach
   YC<-st_transform(YC,st_crs(df))
-  # "st_join, nearest" following https://stackoverflow.com/questions/53854803/calculate-minimum-distance-between-multiple-polygons-with-r
   index<-st_nearest_feature(df, YC) # Creates an ordered list (index) of closest elements from YC shapefile to each feature in "df"
   YC<-YC %>% slice(index) # Subsets the YC shapefile by creating a reordered version based on the element ordering of "index"
   df$YCdist<-as.vector(st_distance(df, YC, by_element = TRUE)) # Calc dist between two sf objects, element-by-element; as.vector removes [m] units
   df<-cbind(df,YC) # Column binding
   rm(YC)
-  # Dropping variables we are not going to use in the analysis (see google doc)
-  df<-subset(df, select = -c(BLFTP_PRK, BLFTP_TRLS, BLUFF, DOG_FRIEND, DUNES, EZ4STROLLE, PCNC_AREA, RKY_SHORE, SNDY_BEACH, TIDEPOOL, VISTOR_CTR, VOLLEYBALL, WLDLFE_VWG, COUNTY, CountyNum, DISTRICT, Descriptio, GEOGR_AREA, ID, LATITUDE, LONGITUDE, LIST_ORDER, LocationMo, NameMobile, PHONE_NMBR, Photo_1, Photo_2, Photo_3, Photo_4, Bch_whlchr, geom))
-  # all "no" to zero, and all "yes" to 1 for indicator variables, Note, there are a range of values here {?, No, Yes, Yes?, yes, no, NO} and we recode these as {0, 0, 1, 1, 1, 0, 0} where 1 == yes and 0 == no.
+  df<-subset(df, select = -c(BLFTP_PRK, BLFTP_TRLS, BLUFF, DOG_FRIEND, DUNES, EZ4STROLLE, PCNC_AREA, RKY_SHORE, SNDY_BEACH, TIDEPOOL, VISTOR_CTR, VOLLEYBALL, WLDLFE_VWG, COUNTY, CountyNum, DISTRICT, Descriptio, GEOGR_AREA, ID, LATITUDE, LONGITUDE, LIST_ORDER, LocationMo, NameMobile, PHONE_NMBR, Photo_1, Photo_2, Photo_3, Photo_4, Bch_whlchr, geom)) # Dropping variables we are not going to use in the analysis (see google doc)
   df<-rapply(df, as.character, classes="factor", how="replace") # Factors as character https://stackoverflow.com/questions/2851015/convert-data-frame-columns-from-factors-to-characters
-  df <- df %>% 
+  df <- df %>% # # all "no" to zero, and all "yes" to 1 for indicator variables, Note, there are a range of values here {?, No, Yes, Yes?, yes, no, NO} and we recode these as {0, 0, 1, 1, 1, 0, 0} where 1 == yes and 0 == no.
     mutate_if(is.character,funs(str_replace(., c("\\?|No|NO|no"), "0"))) # http://www2.stat.duke.edu/~cr173/Sta523_Fa15/regex.html
   df <- df %>% 
     mutate_if(is.character,funs(str_replace(., c("Yes|yes|Yes\\?"), "1")))
-  # Boating facilities (is different from "BOATING", though mostly overlap)
-  table(df$BT_FACIL_T) # An array of facilities, assign 1 if something, 0 if nothing
+  table(df$BT_FACIL_T) # Boating facilities (is different from "BOATING", though mostly overlap). An array of facilities, assign 1 if something, 0 if nothing
   df$BT_FACIL_T<-ifelse(is.na(df$BT_FACIL_T),0,ifelse(df$BT_FACIL_T==0,0,1))
-  # Bike path seems to have a coding error (10 instead of 1)
-  df$BIKE_PATH<-ifelse(df$BIKE_PATH==10,1,df$BIKE_PATH)
+  df$BIKE_PATH<-ifelse(df$BIKE_PATH==10,1,df$BIKE_PATH)  # Bike path seems to have a coding error (10 instead of 1)
   df<-rapply(df, as.numeric, classes="character", how="replace") # Character as numeric
-  # Any NAs? 
-  sapply(df, function(x) sum(is.na(x)))
-  # Replace NAs with zero
-  df[is.na(df)]<-0
+  sapply(df, function(x) sum(is.na(x))) # Any NAs? 
+  df[is.na(df)]<-0 # Replace NAs with zero
   
-  ## Water trail (existing sites as of 2016)
-  WT<-st_read("Data/WaterTrailSites_July2016.gpkg")
+  WT<-st_read("Data/WaterTrailSites_July2016.gpkg") # Water trail (existing sites as of 2016)
   WT<-WT[,c("WaterTrail","geom")]
   WT<-st_transform(WT,st_crs(df))
-  # Spatial join
-  df<-st_join(df,WT)
+  df<-st_join(df,WT)# Spatial join
   df$WaterTrail<-ifelse(is.na(df$WaterTrail),0,df$WaterTrail)
   rm(WT)
   
-  ## Bay trail
-  BT<-st_read("Data/Mar_2018_BayTrail.gpkg")
-  # When were certain sections built? Allow querying
+  BT<-st_read("Data/Mar_2018_BayTrail.gpkg")  # Bay trail
   BT<-BT[which(BT$STATUS=="Existing"),] # Only currently existing projects
   BT$Year_Compl<-as.character(BT$Year_Compl)
   BT$Year_Compl<-ifelse(is.na(BT$Year_Compl),1989,
                         ifelse(BT$Year_Compl=="pre-1989",1989,BT$Year_Compl)) # Assuming all NAs are 1989 or prior
   BT$Year_Compl<-as.numeric(BT$Year_Compl)
-  # Filter by year here
-  #BT<-BT[which(BT$YearCompl<=2004),]
-  # Indicator variable
-  BT$BayTrail<-1
+  #BT<-BT[which(BT$YearCompl<=2004),] # Filter by year here
+  BT$BayTrail<-1 # Indicator variable
   BT<-BT[,c("BayTrail","geom")]
-  # Spatial join
-  df<-st_join(df,BT)
+  df<-st_join(df,BT) # Spatial join
   df<-unique(df) # Two or more lines may exist within one hexagon - only need one
   df$BayTrail[is.na(df$BayTrail)]<-0 # Replacing NAs with zero
   rm(BT)
   
-  ## Distance to nearest marsh
-  DM<-st_read("Data/nlcd_wetlands_2016.gpkg")
+  DM<-st_read("Data/nlcd_wetlands_2016.gpkg") # Distance to nearest marsh. Follows similar approach to road nearest distance.
   DM<-st_transform(DM, st_crs(df))
-  # Nearest distance, following https://stackoverflow.com/questions/53854803/calculate-minimum-distance-between-multiple-polygons-with-r
   system.time(index<-st_nearest_feature(df, DM)) # Creates an ordered list (index) of closest elements from Rd shapefile to each feature in "df"
   DM<-DM %>% slice(index) # Subsets the Rd shapefile by creating a reordered version based on the element ordering of "index"
   df$wtlddist<-as.vector(st_distance(df, DM, by_element = TRUE)) # Calculates distance between two sf objects, element-by-element. as.vector removes [m] units
   rm(DM, index)
   
-  ## Adjacent precipitation
-  rs.f<-list.files("./Data/Prism Precip", full.names = TRUE)
+  rs.f<-list.files("./Data/Prism Precip", full.names = TRUE) #Adjacent precipitation
   rs.t<-rs.f[8:13]
   s.f<-stack(rs.f)
   s.t<-stack(rs.t)
-  # Creating a (8 mile) buffered shapefile of hexagons 
-  Prec.buff8m<-st_buffer(df,dist=12784)
+  Prec.buff8m<-st_buffer(df,dist=12784) # Creating a (8 mile) buffered shapefile of hexagons 
   Prec.buff8m<-st_transform(Prec.buff8m,crs=crs(s.f))
   Prec.buff8m<-subset(Prec.buff8m, select = c(id,geometry))
-  # Extracting mean precip for each buffered polygon 
-  system.time(Prec.buff8m$meanprecf<-rowMeans(exact_extract(s.f, Prec.buff8m, "mean")))
+  system.time(Prec.buff8m$meanprecf<-rowMeans(exact_extract(s.f, Prec.buff8m, "mean"))) # Extracting mean precip for each buffered polygon 
   system.time(Prec.buff8m$meanprect<-rowMeans(exact_extract(s.t, Prec.buff8m, "mean")))
-  # Merging to dataframe
-  Prec.buff8m<-Prec.buff8m[,c("id","meanprecf","meanprect")]
+  Prec.buff8m<-Prec.buff8m[,c("id","meanprecf","meanprect")]  
   Prec.buff8m$geometry<-NULL # Turns sf object into dataframe
-  df<-merge(df,Prec.buff8m,"id")
-  # Removing unneeded vars
-  rm(s.f,s.t,Prec.buff8m,rs.f,rs.t)
+  df<-merge(df,Prec.buff8m,"id") # Merging to dataframe
+  rm(s.f,s.t,Prec.buff8m,rs.f,rs.t) # Removing unneeded vars
   
-  ## Adjacent air temperature
-  rs.f<-list.files("./Data/Prism Mean Temp", full.names = TRUE)
+  rs.f<-list.files("./Data/Prism Mean Temp", full.names = TRUE) # Adjacent air temperature
   rs.t<-rs.f[8:13]
   s.f<-stack(rs.f)
   s.t<-stack(rs.t)
-  # Creating a (8 mile) buffered shapefile of hexagons 
-  AT.buff8m<-st_buffer(df,dist=12784)
+  AT.buff8m<-st_buffer(df,dist=12784) # Creating a (8 mile) buffered shapefile of hexagons
   AT.buff8m<-st_transform(AT.buff8m,crs=crs(s.f))
   AT.buff8m<-subset(AT.buff8m, select = c(id,geometry))
-  # Extracting mean air temp for each buffered polygon 
-  system.time(AT.buff8m$meanatf<-rowMeans(exact_extract(s.f, AT.buff8m, "mean")))
+  system.time(AT.buff8m$meanatf<-rowMeans(exact_extract(s.f, AT.buff8m, "mean"))) # Extracting mean air temp for each buffered polygon 
   system.time(AT.buff8m$meanatt<-rowMeans(exact_extract(s.t, AT.buff8m, "mean")))
-  # Merging to dataframe
   AT.buff8m<-AT.buff8m[,c("id","meanatf","meanatt")]
   AT.buff8m$geometry<-NULL # Turns sf object into dataframe
-  df<-merge(df,AT.buff8m,"id")
-  # Removing unneeded vars
-  rm(AT.buff8m,s.f,s.t,rs.f,rs.t)
+  df<-merge(df,AT.buff8m,"id") # Merging to dataframe
+  rm(AT.buff8m,s.f,s.t,rs.f,rs.t) # Removing unneeded vars
   
-  ## Adjacent sea surface temperature
-  SSTf<-raster("Data/flickr_GIOVANNI-g4.timeAvgMap.MODISA_L3m_SST_Monthly_4km_R2019_0_sst.20050101-20171231.125W_32N_116W_42N.tif")
+  SSTf<-raster("Data/flickr_GIOVANNI-g4.timeAvgMap.MODISA_L3m_SST_Monthly_4km_R2019_0_sst.20050101-20171231.125W_32N_116W_42N.tif") # Adjacent sea surface temperature
   SSTt<-raster("Data/twitter_GIOVANNI-g4.timeAvgMap.MODISA_L3m_SST_Monthly_4km_R2019_0_sst.20120101-20171231.125W_32N_116W_42N.tif")
-  # Creating a (15km) buffered shapefile of hexagons
-  SST.buff<-st_buffer(df,dist=15000)
+  SST.buff<-st_buffer(df,dist=15000) # Creating a (15km) buffered shapefile of hexagons
   SST.buff<-st_transform(SST.buff,crs=crs(SSTf))
-  # Extracting mean sea surface temp for each buffered polygon
-  system.time(SST.buff$meanSSTf<-exact_extract(SSTf, SST.buff, "mean"))
+  system.time(SST.buff$meanSSTf<-exact_extract(SSTf, SST.buff, "mean")) # Extracting mean sea surface temp for each buffered polygon
   system.time(SST.buff$meanSSTt<-exact_extract(SSTt, SST.buff, "mean"))
-  # Merging to dataframe
-  SST.buff<-SST.buff[,c("id","meanSSTf","meanSSTt")]
+  SST.buff<-SST.buff[,c("id","meanSSTf","meanSSTt")] 
   SST.buff$geometry<-NULL # Turns sf object into dataframe
-  df<-merge(df,SST.buff,"id")
-  # Removing unneeded vars
-  rm(SST.buff,SSTf,SSTt)
+  df<-merge(df,SST.buff,"id") # Merging to dataframe
+  rm(SST.buff,SSTf,SSTt) # Removing unneeded vars
   
-  ## Transferring to long format with flickr/twitter id (PUD vs TUD)
+  # Transferring to long format with flickr/twitter id (PUD vs TUD)
   df<-gather(df,source,ud,c(PUD,TUD))
   df$meanprec<-ifelse(df$source=="PUD",df$meanprecf,df$meanprect)
   df$meanat<-ifelse(df$source=="PUD",df$meanatf,df$meanatt)
@@ -321,7 +269,29 @@ results %>%
 
 rm(results2)
 
-write.csv(results,"lresults.csv",row.names = FALSE)
+## Additional data processing
+results$ud[is.na(results$ud)]<-0 # Replacing NAs in cell data with zero. Comment out and let the next line drop if we want to drop them
+#df<-df[complete.cases(df),] # Doesn't work with sf objects
+results<-results[!sf::st_is_empty(results), ] %>% na.omit() # https://stackoverflow.com/questions/52173746/return-complete-cases-of-sf-object-in-r drops a few observations where the precipitation and air temp rasters returned NAs due to inadequate coverage, results in different num obs for cell data, as 2019 coverage isn't as complete as the mean values derived from the longer time series relevant for the twitter and flickr data
+# rescaling population to thousands and distances to km, to deal with model convergence issues
+results$sumpop<-results$sumpop/1000
+results$rdist<-results$rdist/1000
+results$wtlddist<-results$wtlddist/1000
+
+# Filtering area of influence of amenities represented in Yourcoast database
+results<-mutate_at(results, vars(BIKE_PATH,BOATING,BT_FACIL_T,CAMPGROUND,DSABLDACSS,FEE,FISHING,PARKING,PTH_BEACH,RESTROOMS,STRS_BEACH), list(~ ifelse(YCdist>1000, 0, .)))
+
+# ESI crosswalk
+results$RockyShore<-results$x1a + results$x2a + results$x6a +results$x8a
+results$SandyBeach<-results$x3a + results$x3b + results$x4 + results$x5
+results$Marshes<-results$x9a + results$x9b + results$x10a
+results$Armored<-results$x1b + results$x6b + results$x8b + results$x8c
+
+## Write to disk
+results %>% 
+  st_drop_geometry() %>% 
+  write.csv("lresults.csv",row.names = FALSE)
+
 st_write(results,"lresults.gpkg")
 
 #unlink("./Data", recursive = TRUE) # Delete data directory
