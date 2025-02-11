fe31ff56ab1f8af8afeaa14543fd7fde56bb76a5
Robert Griffin <r.maurice.griffin@gmail.com>
2025-02-10 20:33:07 -0500

    psv file deletion is done based on the names of the files downloaded, versus scanning the folder for files ending in *.psv. Attempt to deal with suspected race conditions during parallel execution.

--- TradeAreas.R
+++ TradeAreas.R
@@ -8,7 +8,7 @@ library(renv)
 ## Packages
 #Sys.setenv(RENV_PATHS_RTOOLS = "C:/rtools40/") # https://github.com/rstudio/renv/issues/225
 
-PKG <- c("R.utils","httr","tidyverse","jsonlite","sf","geojsonsf","lwgeom","furrr","arrow") 
+PKG<-c("R.utils","httr","tidyverse","jsonlite","sf","geojsonsf","lwgeom","furrr","arrow") 
 
 for (p in PKG) {
   if(!require(p,character.only = TRUE)) {  
@@ -21,15 +21,15 @@ renv::snapshot()
 # Prep to access the API -------------------------------------------------
 api_key<-read.csv(file = "APIkey.csv", header = FALSE) 
 api_key<-api_key$V1
-headers <- add_headers(Authorization = api_key, `Content-Type` = "application/json") # Set up the headers including the API key
-url <- "https://api.gravyanalytics.com/v1.1/areas/tradeareas" # API URL to query
+headers<-add_headers(Authorization = api_key, `Content-Type` = "application/json") # Set up the headers including the API key
+url<-"https://api.gravyanalytics.com/v1.1/areas/tradeareas" # API URL to query
 
 # Prep for the GIS layer --------------------------------------------------
 if (!dir.exists("tData")) { # Create data directory if it doesn't exist
   dir.create("tData", recursive = TRUE)
 }
 
-df<-st_read("Data/PUD_2005-2017_1000m.gpkg") %>% dplyr::select(PUD_YR_AVG) # Load data
+df<-st_read("Data/PUD_2005-2017_250m.gpkg") %>% dplyr::select(PUD_YR_AVG) # Load data
 df<-st_transform(df, crs = 4326) # Needs to be projected in 4326 to work with lat long conventions of the API
 df<-st_cast(df,"POLYGON") # Switch from multi-polygon to polygon
 #df<-st_make_valid(df) # Adjust geometries to right-hand rule (doesn't seem mandatory)
@@ -40,6 +40,8 @@ batchapi<-function(dft,s,e,fname){ # Function converts sf object to json, passes
   
   dft$startDateTimeEpochMS<-s # 1704067200000 These don't seem to work as query variables
   dft$endDateTimeEpochMS<-e # 1706831999000 Says endDateTimeEpochMS must be within 90 days from startDateTimeEpochMS disregarding time of day, but this doesn't seem true. Any date is possible.
+  # dft$startDateTimeEpochMS<-1704067200000
+  # dft$endDateTimeEpochMS<-1735689599999
   dft<-dft %>% select(-PUD_YR_AVG) # Need more than the geometry column to create a feature collection using sf_geojson. Also, there is a limit of 20 features per request (even if it doesn't return results for 20 features).
   dftj<-sf_geojson(dft,atomise = FALSE) # Convert sf object to GeoJSON 
   
@@ -47,7 +49,7 @@ batchapi<-function(dft,s,e,fname){ # Function converts sf object to json, passes
   dftj<-toJSON(dftj, auto_unbox = TRUE)
   
   # Export query (asynchronous)
-  system.time(response <- POST(url, headers, body = dftj, encode = "json", query = list(
+  system.time(response<-POST(url, headers, body = dftj, encode = "json", query = list(
     # includeHeaders = FALSE, # Remove headers - potentially useful for batching
     # returnDeviceCountByGeoHash = TRUE, # "If true, the geoHashDeviceCount and geoHashWidthHeights fields are populated per feature" - don't see this. It does return "searchobjectid" in the response psv that corresponds to a given "id" in the json properties
     #decisionLocationTypes = list(c("LATLNG","CBG")),
@@ -59,19 +61,19 @@ batchapi<-function(dft,s,e,fname){ # Function converts sf object to json, passes
     responseType = "EXPORT"  # Requesting an export response
   )))
   
-  requestID <- response$headers$requestid
-  status_url <- paste0("https://api.gravyanalytics.com/v1.1/requestStatus/", requestID)
-  export_complete <- FALSE
+  requestID<-response$headers$requestid
+  status_url<-paste0("https://api.gravyanalytics.com/v1.1/requestStatus/", requestID)
+  export_complete<-FALSE
   
   # Function that pings the API to see if the export request is done every 1 seconds and returns either of {files ready, still waiting, failed}
   while (!export_complete) {
     Sys.sleep(1)  # Wait for 1 seconds before polling again
-    status_response <- GET(status_url, add_headers(Authorization = api_key))
-    status_content <- content(status_response, "parsed")
+    status_response<-GET(status_url, add_headers(Authorization = api_key))
+    status_content<-content(status_response, "parsed")
     
     if (status_content$status == "DONE") {
-      export_complete <- TRUE
-      aws_s3_link <- as.character(status_content$presignedUrlsByDataType$tradeAreas)
+      export_complete<-TRUE
+      aws_s3_link<-as.character(status_content$presignedUrlsByDataType$tradeAreas)
       base::cat("Your files are ready")
     } else if (status_content$status == "FAILED") {
       stop("Export request failed. Please try again.")
@@ -82,18 +84,21 @@ batchapi<-function(dft,s,e,fname){ # Function converts sf object to json, passes
   
   # Loading export results into workspace -----------------------------------
   
-  file_name <- sub("\\?.*", "", basename(aws_s3_link)) # Extracting the file name
+  file_name<-sub("\\?.*", "", basename(aws_s3_link)) # Extracting the file name
   
-  lapply(seq_along(aws_s3_link), function(i) { # Batch downloading all links returned by the API call. Mode = "wb" is important.
-    download.file(aws_s3_link[i], destfile = file.path("tData", file_name[i]), mode = "wb")
+  downloaded_files<-lapply(seq_along(aws_s3_link), function(i) { # Batch downloading all links returned by the API call. Mode = "wb" is important.
+    file_path<-file.path("tData", file_name[i]) # Construct full path
+    download.file(aws_s3_link[i], destfile = file_path, mode = "wb") # Download file
+    return(file_path) # Return the file path
   })
+  downloaded_files<-unlist(downloaded_files)
   
   xp<-do.call(rbind, # Row bind files into a dataframe 
               lapply( # Apply over all elements in a list
-                file.path("tData", sub("\\.gz$", "", file_name)), # Elements in a list that are named based on the API call  
+                file.path("tData/", sub("\\.gz$", "", file_name)), # Elements in a list that are named based on the API call  
                 function(file) {read.csv(file, sep = "|", header = TRUE)})) # Reading files in
   
-  invisible(file.remove(list.files(path = "tData", pattern = "\\.psv$", full.names = TRUE)))
+  invisible(unlink(downloaded_files)) # Deleting downloaded psv files
   
   # Ensure xp is not NULL before proceeding
   if (is.null(xp) || nrow(xp) == 0) {
